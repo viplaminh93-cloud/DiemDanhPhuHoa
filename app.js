637fc9872c36da1f79d7420a76ccdf86ea97b5a2
viplaminh93-cloud <viplaminh93@gmail.com>
2026-07-11 15:42:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -120,86 +120,6 @@ function capNhatTongTuServer(loai){
 
 
 
-//======================
-// Camera
-//======================
-
-async function startCamera(){
-
-    if(scanner){
-
-        try{
-
-            await scanner.stop();
-
-            scanner.clear();
-
-        }catch(e){}
-
-    }
-
-    scanner = new Html5Qrcode("reader");
-
-    try{
-
-        await scanner.start(
-
-            { facingMode:"environment" },
-
-            {
-
-                fps:10,
-
-                qrbox:{
-                    width:220,
-                    height:220
-                },
-
-                rememberLastUsedCamera:true,
-
-                disableFlip:true
-
-            },
-
-            qrSuccess
-
-        );
-
-    }catch(err){
-
-        alert("Không mở được camera\n\n" + err);
-
-    }
-
-}
-
-
-
-//======================
-// Quét thành công
-//======================
-
-function qrSuccess(text){
-
-    if(dangXuLy) return;
-
-    dangXuLy = true;
-
-    if(scanner){
-
-        scanner.pause(true);
-
-    }
-
-    if(navigator.vibrate){
-
-        navigator.vibrate(50);
-
-    }
-
-    guiDiemDanh(text);
-
-}
 
 
 
@@ -505,38 +425,6 @@ window.onload = function(){
 };
 
 
-
-//======================
-// Quay lại
-//======================
-
-async function backHome(){
-
-    try{
-
-        if(scanner){
-
-            await dongBoQueue();
-
-            await scanner.stop();
-
-            scanner.clear();
-
-            scanner = null;
-
-        }
-
-    }catch(e){}
-
-    dangXuLy = false;
-
-    document.querySelector(".home")
-        .style.display = "block";
-
-    document.getElementById("scannerBox")
-        .classList.add("hidden");
-
-}
 
 
 
