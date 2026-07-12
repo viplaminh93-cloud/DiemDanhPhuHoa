85b270786d7514d9b2ad478173b93684c2a78332
viplaminh93-cloud <viplaminh93@gmail.com>
2026-07-12 20:23:37 +0700

    Create constant.js

---
+++ js/constant.js
@@ -0,0 +1,41 @@
+"use strict";
+
+//========================
+// STATUS
+//========================
+
+const STATUS={
+
+    SUCCESS:"success",
+
+    DUPLICATE:"duplicate",
+
+    ERROR:"error"
+
+};
+
+//========================
+// EVENT
+//========================
+
+const EVENT={
+
+    QR_SUCCESS:"qr-success",
+
+    POPUP_CLOSE:"popup-close"
+
+};
+
+//========================
+// LOAI DIEM DANH
+//========================
+
+const LOAI={
+
+    LE:"Dự lễ",
+
+    GIAOLY:"Giáo lý"
+
+};
+
+console.log("constants.js loaded");
