2ffb5e1795056827ffa02589cc586f6ec037d13c
viplaminh93-cloud <viplaminh93@gmail.com>
2026-07-20 08:46:53 +0700

    Update report.renderer.js

--- report/report.renderer.js
+++ report/report.renderer.js
@@ -12,31 +12,9 @@ const ReportRenderer = (()=>{
     //----------------------------------
 
     function renderSummary(data){
-
-        Renderer.text(
-
-            "tongCount",
-
-            data.total || 0
-
-        );
-
-        Renderer.text(
-
-            "leCount",
-
-            data.le || 0
-
-        );
-
-        Renderer.text(
-
-            "glCount",
-
-            data.giaoly || 0
-
-        );
-
+        Renderer.text("tongCount", data.total || 0);
+        Renderer.text("leCount", data.le || 0);
+        Renderer.text("glCount", data.giaoly || 0);
     }
 
     //----------------------------------
@@ -44,86 +22,32 @@ const ReportRenderer = (()=>{
     //----------------------------------
 
     function renderList(list){
-
         let html = "";
-
-        list.forEach(item=>{
-
-            html += `
-
+        list.forEach(item=>{html += `
             <div class="report-row">
-
                 <div class="report-left">
-
-                    <div class="report-name">
-
-                        ${item.hoten}
-
-                    </div>
-
-                    <div class="report-class">
-
-                        ${item.lop}
-
-                    </div>
-
+                    <div class="report-name">${item.hoten}</div>
+                    <div class="report-class">${item.lop}</div>
                 </div>
 
                 <div class="report-right">
-
-                    <div class="report-type">
-
-                        ${item.loai}
-
-                    </div>
-
-                    <div class="report-time">
-
-                        ${item.gio}
-
-                    </div>
-
+                    <div class="report-type">${item.loai}</div>
+                    <div class="report-time">${item.gio}</div>
                 </div>
+            </div>`;});
 
-            </div>
+        if(html===""){html=`
+            <div class="report-row">Chưa có dữ liệu.</div>`;
+                     }
 
-            `;
-
-        });
-
-        if(html===""){
-
-            html=`
-
-            <div class="report-row">
-
-                Chưa có dữ liệu.
-
-            </div>
-
-            `;
-
-        }
-
-        Renderer.html(
-
-            "reportList",
-
-            html
-
-        );
-
+        Renderer.html("reportList",html);
     }
 
+
+    
     //----------------------------------
 
-    return{
-
-        renderSummary,
-
-        renderList
-
-    };
+    return{renderSummary, renderList };
 
 })();
 
